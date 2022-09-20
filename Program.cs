5cda2e1e0e9e89ef6b4c08ae0fb00bc4a124b8d2
Wallace Tácio <wallacett.dev@gmail.com>
2022-09-20 11:04:05 -0300

    Adiciona Solução ao exercício Múltiplo de 5 FATEC 2022/2

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Digite um número: ");
+
+double num = Convert.ToDouble(Console.ReadLine());
+
+if(num%5 == 0){
+    Console.WriteLine($"O número {num} é múltiplo de 5.");
+} else {
+    Console.WriteLine($"O número {num} não é múltiplo de 5.");
+}
+Console.ReadKey();
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # multiploDe5
 Exercício múltiplo de 5 Fatec 2022/2
+
+Criar um programa que permita que o usuário digite um número, verificar se o mesmo é múltiplo de 5.
